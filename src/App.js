639439c28148dcb9d0a6d18d14371a117da4dabe
JakubW808 <jakub.wojtowicz808@gmail.com>
2023-05-10 16:25:54 +0200

    PopupForm_Problem[FIXED]
    Przeniesienie stanu z poziomu globalnego do poziomu komponentu PopupForm

--- src/App.js
+++ src/App.js
@@ -2,88 +2,98 @@ import './App.css';
 import React, { useState } from 'react';
 import todo_data from './test';
 import "./components.css"
+import {nanoid} from 'nanoid'
 
 function App() {
 
   const [activeProjectId, setActiveProjectId] = useState(0)
-  const [addTaskForm, setAddTaskForm] = useState(
-    {
-                id: null,
-                title: null,
-                desc: null,
-                status: null
-            }
-  )
 
   function ProjectList() {
 
     function handleProjectChange(event) {
-        const newActProject = todo_data.indexOf(todo_data.find(elem => {
-            return (
-                elem.project_id == event.target.getAttribute("id")
-            )
-        }))
+      const newActProject = todo_data.indexOf(todo_data.find(elem => {
+        return (
+          elem.project_id == event.target.getAttribute("id")
+        )
+      }))
       setActiveProjectId(newActProject)
     }
 
     return (
-        <ul>
-            {
-                todo_data.map(project => {
-                  return <li key={project.project_id}><a id={project.project_id} onClick={handleProjectChange}>{project.project_name}</a></li>
-                })
+      <ul>
+        {
+          todo_data.map(project => {
+            return <li key={project.project_id}><a id={project.project_id} onClick={handleProjectChange}>{project.project_name}</a></li>
+          })
         }
         <li><a>+ Dodaj nowy projekt</a></li>
-        </ul>
+      </ul>
     )
-}
+  }
 
-function Navbar() {
+  function Navbar() {
     return (
-        <div className="navibar">
-            <p id="logo">Todo</p>
-            <ul>
-                <li><a>Jakaś opcja tu będzie</a></li>
-                <li><a>Jakaś opcja tu będzie</a></li>
-                <li><a>Jakaś opcja tu będzie</a></li>
-            </ul>
-            <p>Projekty:</p>
-        <ProjectList/>
-        </div>
+      <div className="navibar">
+        <p id="logo">Todo</p>
+        <ul>
+          <li><a>Jakaś opcja tu będzie</a></li>
+          <li><a>Jakaś opcja tu będzie</a></li>
+          <li><a>Jakaś opcja tu będzie</a></li>
+        </ul>
+        <p>Projekty:</p>
+        <ProjectList />
+      </div>
     )
-}
+  }
   
-function Task(prop) {
+  function Task(prop) {
     return (
       <div className={"task_card"}>
         <div className="task_name">{prop.name}</div>
-        <div className={'task_status '+prop.status} title={prop.status}></div>
+        <div className={'task_status ' + prop.status} title={prop.status}></div>
         <div className="task_desc">{prop.desc}</div>
-        </div>
+      </div>
     )
-}
+  }
 
   function handleTaskForm() {
-      const oldDisplay = document.getElementById("overlay").style.display
-      const newDisplay = (oldDisplay == "none" ? "flex" : "none")
-      document.getElementById("overlay").style.display = newDisplay
+    const oldDisplay = document.getElementById("overlay").style.display
+    const newDisplay = (oldDisplay == "none" ? "flex" : "none")
+    document.getElementById("overlay").style.display = newDisplay
   }
   
   function PopUpForm(prop) {
 
+    const [addTaskForm, setAddTaskForm] = useState(
+    {
+      id: nanoid(),
+      title: null,
+      desc: null,
+      status: "todo"
+    }
+  )
+
     function handlePopUpFormChange(event) {
-      console.log("Początek wykonania kodu")
-      console.log()
+
       let formName = event.target.id;
       let formValue = event.target.value;
 
-        const newTask = { ...addTaskForm };
-        newTask[formName] = formValue;
+      const newTask = { ...addTaskForm };
+      newTask[formName] = formValue;
 
       setAddTaskForm(newTask)
-      console.log("Koniec wykonania kodu")
+      console.log(addTaskForm)
     }
 
+    const handleSubmit = (event) => {
+      event.preventDefault();
+
+      console.log(addTaskForm)
+      addTaskForm.id = nanoid()
+      handleTaskForm();
+    }
+
+    
     return (
       <div id="pop-up">
         <form>
@@ -95,8 +105,8 @@ function Task(prop) {
             <option value="in_progress">In Progress</option>
             <option value="done">Done</option>
           </select>
-          <button type="reset" /*onClick={handleTaskForm}*/>{prop.button}</button>
-          <button type="reset" /*onClick={handleTaskForm}*/>Anuluj</button>
+          <button type="reset" onClick={handleSubmit}>{prop.button}</button>
+          <button type="reset" onClick={handleTaskForm}>Anuluj</button>
         </form>
       </div>
     )
@@ -104,38 +114,38 @@ function Task(prop) {
   
   function Task_list(prop) {
   
+    return (
+      <div className="task_table_body">
+        <div className='task_title'><h2>{todo_data[prop.index].project_name}</h2></div>
+        {(todo_data[prop.index].task_list).map((task) =>
+          <Task
+            key={task.id}
+            name={task.title}
+            desc={task.desc}
+            status={task.status}
+          />
+        )
+        }
+        <div className="task_title add_task" onClick={handleTaskForm}>
+          <h2>Dodaj nowy task</h2>
+        </div>
+        
+      </div>
+    )
+  }
+
   return (
-    <div className="task_table_body">
+    <div className="App">
+      <Navbar />
+      <Task_list
+        index={activeProjectId}
+      />
       <div id="overlay">
         <PopUpForm
           title="Dodaj nowy task"
           button="Dodaj"
         />
       </div>
-      <div className='task_title'><h2>{todo_data[prop.index].project_name}</h2></div>
-            {(todo_data[prop.index].task_list).map((task) =>
-                <Task
-                    key={task.id}
-                    name={task.title}
-                    desc={task.desc}
-                    status={task.status}
-                />
-            )
-        }
-        <div className="task_title add_task" onClick={handleTaskForm}>
-          <h2>Dodaj nowy task</h2>
-        </div>
-        
-        </div>
-    )
-}
-
-  return (
-    <div className="App">
-      <Navbar />
-      <Task_list
-      index = {activeProjectId}
-      />
     </div>
   );
 }
