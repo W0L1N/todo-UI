9c40a9c2fb6518e2153e486a975075f80fb53292
JakubW808 <jakub.wojtowicz808@gmail.com>
2023-05-10 13:16:49 +0200

    Przed wprowadzeniem dodawania 'todo' do projektów
    -Wszystkie komponenty umieściłem w App.js
    -Pousuwałem śmieciowe pliki
    -Zmieniłem trochę CSS strony
    -Dodałem możliwość poruszania się po projektach
    -Poprawiłem wygląd komponentu 'Todo' i poprawiłem sposób renderowania

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import todo_data from './test';
+import "./components.css"
 
 function App() {
+
+  const [activeProjectId, setActiveProjectId] = useState(0)
+
+  function ProjectList() {
+
+    function handleProjectChange(event) {
+        const newActProject = todo_data.indexOf(todo_data.find(elem => {
+            return (
+                elem.project_id == event.target.getAttribute("id")
+            )
+        }))
+      setActiveProjectId(newActProject)
+    }
+
+    return (
+        <ul>
+            {
+                todo_data.map(project => {
+                  return <li key={project.project_id}><a id={project.project_id} onClick={handleProjectChange}>{project.project_name}</a></li>
+                })
+        }
+        <li><a>+ Dodaj nowy projekt</a></li>
+        </ul>
+    )
+}
+
+function Navbar() {
+    return (
+        <div className="navibar">
+            <p id="logo">todo</p>
+            <ul>
+                <li><a>Jakaś opcja tu będzie</a></li>
+                <li><a>Jakaś opcja tu będzie</a></li>
+                <li><a>Jakaś opcja tu będzie</a></li>
+            </ul>
+            <p>Projekty:</p>
+        <ProjectList/>
+        </div>
+    )
+}
+  
+function Todo(prop) {
+    return (
+      <div className={"todo_card"}>
+        <div className="todo_name">{prop.name}</div>
+        <div className={'todo_status '+prop.status} title={prop.status}></div>
+        <div className="todo_desc">{prop.desc}</div>
+        </div>
+    )
+}
+
+function Todo_list(prop) {
+    const todo_table = Object.values({ ...todo_data })
+  return (
+    <div className="todo_table_body">
+       <div className='todo_title'><h2>{todo_table[prop.index].project_name}</h2></div>
+            {(todo_table[prop.index].todo_list).map((todo) =>
+                <Todo
+                    key={todo.id}
+                    name={todo.name}
+                    desc={todo.desc}
+                    status={todo.status}
+                />
+            )
+        }
+        <div className="todo_title add_card">
+          <h2>Dodaj nowy todo</h2>
+        </div>
+        
+        </div>
+    )
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Todo_list
+      index = {activeProjectId}
+      />
     </div>
   );
 }
---
+++ src/test.js
@@ -0,0 +1,61 @@
+var todo_data = [
+    {
+        project_id: 1,
+        project_name: "Project1",
+        desc: "project description",
+        todo_list: [
+            {
+                id: 1,
+                name: "something to do",
+                desc: "desc of something to do",
+                status: "todo"
+            }
+            
+        ]
+    },
+    {
+        project_id: 2,
+        project_name: "Project2",
+        desc: "project description",
+        todo_list: [
+            {
+                id: 1,
+                name: "something to do",
+                desc: "desc of something to do",
+                status: "todo"
+            },
+            {
+                id: 2,
+                name: "something to do",
+                desc: "desc of something to do",
+                status: "in_progress"
+            },
+        ]
+    },
+    {
+        project_id: 3,
+        project_name: "Project3",
+        desc: "project description",
+        todo_list: [
+            {
+                id: 1,
+                name: "something to do",
+                desc: "desc of something to do",
+                status: "todo"
+            },
+            {
+                id: 2,
+                name: "something to do",
+                desc: "desc of something to do",
+                status: "in_progress"
+            },
+            {
+                id: 3,
+                name: "something to do",
+                desc: "desc of something to do",
+                status: "done"
+            }
+        ]
+    },
+]
+export default todo_data
